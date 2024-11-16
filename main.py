79af185d340b5f86bd24aca93e24fd1bc9541039
OTiaggo <toliveir123@gmail.com>
2024-11-16 09:41:53 -0300

    Update main.py
    Selecionar a key da api da openai

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()  
 
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_KEY')
+
 import asyncio
 from langchain_openai import ChatOpenAI
 from typing import List
